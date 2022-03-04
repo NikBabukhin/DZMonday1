2ccd1be1d1c64867c32faca0ba4db76731a25623
NikBabukhin <6opsh50@gmail.com>
2022-03-04 19:44:56 +0300

    complete task UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {NewComponent} from "./NewComponent";
 import {Button} from "./components/Button";
@@ -12,26 +12,43 @@ function App() {
         {manufacturer: 'Audi', model: 'rs6'}
     ];
 
-    const Button1Foo=(subscriber:string, age:number, address:string) => {
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
         console.log(`Hi ${subscriber}, i'm ${age} y.o, my address is ${address}`)
     }
 
-    const Button2Foo=(name:string)=>{
+    const Button2Foo = (name: string) => {
         console.log(`Hi ${name}`)
     }
 
-    const Button3Foo=()=> {
+    const Button3Foo = () => {
         console.log("i'm stupid button")
     }
+
+    let [counter, setCounter] = useState(0);
+
+    const changeCounter = (id: number) => {
+        if (id === 1) {
+            setCounter(++counter)
+        } else if (id === 2) {
+            setCounter(counter = 0)
+        } else {
+            setCounter(--counter)
+        }
+    }
+
 
     return (
         <div className="App">
             <Header titleHeader={'New Header'}/>
             <Body titleBody={'New Body'}/>
             <NewComponent contentComponent={topCars}/>
-            <Button name={'YouTube Channel 1'} callback={()=>Button1Foo('Vasya', 24, 'Moscow, New Arbat st.')}/>
-            <Button name={'YouTube Channel 2'} callback={()=>Button2Foo('Petya')}/>
+            <Button name={'YouTube Channel 1'} callback={() => Button1Foo('Vasya', 24, 'Moscow, New Arbat st.')}/>
+            <Button name={'YouTube Channel 2'} callback={() => Button2Foo('Petya')}/>
             <Button name={'Stupid button'} callback={Button3Foo}/>
+            <h3>{counter}</h3>
+            <button id={'1'} onClick={() => changeCounter(1)}>+</button>
+            <button id={'2'} onClick={() => changeCounter(2)}>0</button>
+            <button id={'3'} onClick={() => changeCounter(3)}>-</button>
         </div>
     );
 }
