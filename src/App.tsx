08ee35d588a9fb5df09b690b8992226b1a25442c
NikBabukhin <6opsh50@gmail.com>
2022-03-06 19:10:44 +0300

    Do some micro-tasks

--- src/App.tsx
+++ src/App.tsx
@@ -8,61 +8,50 @@ import {Banknotes} from "./components/Banknotes";
 
 function App() {
     const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+        {banknotes: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknotes: 'RUB', value: 100, number: ' w1234567890'},
+        {banknotes: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknotes: 'RUB', value: 100, number: ' r1234567890'},
+        {banknotes: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknotes: 'RUB', value: 50, number: ' v1234567890'},
     ])
 
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
-    ];
-
-    const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(`Hi ${subscriber}, i'm ${age} y.o, my address is ${address}`)
+    type moneyTypes = {
+        banknotes: string
+        value: number
+        number: string
     }
 
-    const Button2Foo = (name: string) => {
-        console.log(`Hi ${name}`)
+    const [filter, setFilter] = useState('all')
+    let currentMoney = money
+
+    const deleteBanknote = (idElement: string) => {
+        let filteredMoney = currentMoney.filter((el: moneyTypes) => el.number !== idElement)
+        setMoney(filteredMoney);
     }
 
-    const Button3Foo = () => {
-        console.log("i'm stupid button")
-    }
-
-    let [counter, setCounter] = useState(0);
-
-    const changeCounter = (id: number) => {
-        if (id === 1) {
-            setCounter(++counter)
-        } else if (id === 2) {
-            setCounter(counter = 0)
+    const setFilterMoney = (valueFilter: string) => {
+        setFilter(valueFilter);
+        if (filter === 'RUB') {
+            currentMoney = money.filter((el: moneyTypes) => el.banknotes === 'RUB')
+            console.log(filter, valueFilter)
+        } else if (filter === 'Dollars') {
+            currentMoney = money.filter((el: moneyTypes) => el.banknotes === 'Dollars')
+            console.log(filter, valueFilter)
         } else {
-            setCounter(--counter)
+            currentMoney = money;
+            console.log(filter, valueFilter)
         }
+        setMoney(currentMoney)
     }
 
-
+    console.log(money, currentMoney);
     return (
-        <div className="App">
-            <Header titleHeader={'New Header'}/>
-            <Body titleBody={'New Body'}/>
-            <NewComponent contentComponent={topCars}/>
-            <Button name={'YouTube Channel 1'} callback={() => Button1Foo('Vasya', 24, 'Moscow, New Arbat st.')}/>
-            <Button name={'YouTube Channel 2'} callback={() => Button2Foo('Petya')}/>
-            <Button name={'Stupid button'} callback={Button3Foo}/>
-            <h3>{counter}</h3>
-            <button id={'1'} onClick={() => changeCounter(1)}>+</button>
-            <button id={'2'} onClick={() => changeCounter(2)}>0</button>
-            <button id={'3'} onClick={() => changeCounter(3)}>-</button>
-            <Banknotes/>
-        </div>
+        <div className="App"><Banknotes dataBanknotes={currentMoney}
+                                        callbackFuncDelete={deleteBanknote}
+                                        callbackFuncSortAll={setFilterMoney}/></div>
     );
 }
 
--- src/components/Banknotes.tsx
+++ src/components/Banknotes.tsx
@@ -1,11 +1,46 @@
-export const Banknotes = () => {
+import {useState} from "react";
+
+type BanknotesPropsType = {
+    banknotes: string,
+    value: number,
+    number: string,
+}
+type BanknotesPropsTypeAll = {
+    dataBanknotes: Array<BanknotesPropsType>
+    callbackFuncDelete: (el: string) => void
+    callbackFuncSortAll: (el: string) => void
+}
+
+
+export const Banknotes = (props: BanknotesPropsTypeAll) => {
+
+    const onClickHandlerDelete = (idElement: string) => {
+        props.callbackFuncDelete(idElement);
+    }
+    const onClickHandlerSort = (el:string) => {
+        props.callbackFuncSortAll(el)
+        console.log(props.dataBanknotes, el);
+    }
+
     return (
         <div>
-            <li>
-                <span>Banknote</span>
-                <span>Nominal</span>
-                <span>Ser. Number</span>
-            </li>
+            <ul>
+                {props.dataBanknotes.map((el, index: number) => {
+                    return (
+                        <li key={index}>
+                            <span>{el.banknotes} </span>
+                            <span>{el.value} </span>
+                            <span>{el.number} </span>
+                            <button onClick={() => onClickHandlerDelete(el.number)}>Delete banknote</button>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div>
+                <button onClick={()=>onClickHandlerSort('Dollars')}>Show Dollars</button>
+                <button onClick={()=>onClickHandlerSort('RUB')}>Show Rubles</button>
+                <button onClick={()=>onClickHandlerSort('all')}>Show All</button>
+            </div>
         </div>
     )
 }
